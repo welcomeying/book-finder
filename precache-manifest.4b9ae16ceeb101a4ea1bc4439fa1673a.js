a732cf7292d42cba3d8bec5b95d55a1f76b21963
Ying Huang <yinghuang1016@gmail.com>
2019-03-01 00:06:58 -0500

    Updates

---
+++ precache-manifest.4b9ae16ceeb101a4ea1bc4439fa1673a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "736fd4229ce5946055cf",
+    "url": "/book-finder/static/js/runtime~main.736fd422.js"
+  },
+  {
+    "revision": "304fcb6b116eb7565763",
+    "url": "/book-finder/static/js/main.304fcb6b.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/book-finder/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "304fcb6b116eb7565763",
+    "url": "/book-finder/static/css/main.1c59abf5.chunk.css"
+  },
+  {
+    "revision": "235d3974c8afa6f37a3acfe226ecd625",
+    "url": "/book-finder/index.html"
+  }
+];
