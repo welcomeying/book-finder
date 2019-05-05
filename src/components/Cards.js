6b93371b7955b274289a658e79f4aa88cbbb9d81
Ying Huang <yinghuang1016@gmail.com>
2019-05-05 17:15:46 -0400

    Add mobile responsiveness

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -64,19 +64,19 @@ class Cards extends Component {
       <span title='Save to My Bookshelf' className='save-star'>&#9734;</span>;
     return (
       <div className='book-card'>
-        <div className='book-cover'>
+        
           <img className='book-img' src={this.props.imageLink} alt={this.props.bookTitle} />
-        </div>
+        
         <div className='book-info'>
           <div className='book-title'>{this.props.bookTitle}</div>
           <div className='book-author'>By: {this.props.bookAuthor}</div>
           <div className='book-publisher'>Published By: {this.props.bookPublisher}</div>
           <a href={this.props.bookLink} target='_blank' rel='noopener noreferrer'>
           <button className='book-link'>See this Book</button></a>
+          <div className='save-book' onClick={this.toggleSave}>
+          {saveIcon}
+          </div> 
         </div>
-        <div className='save-book' onClick={this.toggleSave}>
-        {saveIcon}
-        </div> 
       </div>
     );
   }
